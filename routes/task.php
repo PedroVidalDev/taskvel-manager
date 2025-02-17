1aa0d427f1f53e3e736fd8f50a29c0f973ba0b3e
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-17 09:26:35 -0300

    refactor: change task endpoints

--- routes/task.php
+++ routes/task.php
@@ -5,10 +5,10 @@ namespace Routes;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/task')->group(function () {
+Route::prefix('/tasks')->group(function () {
     Route::get('/', [TaskController::class, 'index']);
     Route::get('/{id}', [TaskController::class, 'show']);
-    Route::post('/store', [TaskController::class, 'store']);
-    Route::post('/{id}', [TaskController::class, 'update']);
-    Route::get('/{id}', [TaskController::class, 'destroy']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
+    Route::post('/', [TaskController::class, 'store']);
 });
