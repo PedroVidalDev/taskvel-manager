05a9b3c4332133bea0655e294471ce3d7386175e
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-17 11:50:28 -0300

    feat: add routes and methods to add and retrieve a subtasks

--- routes/task.php
+++ routes/task.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/tasks')->group(function () {
     Route::get('/', [TaskController::class, 'index']);
     Route::get('/{id}', [TaskController::class, 'show']);
+    Route::get('/{id}/subtasks', [TaskController::class, 'getAllSubtasks']);
     Route::put('/{id}', [TaskController::class, 'update']);
     Route::delete('/{id}', [TaskController::class, 'destroy']);
     Route::post('/', [TaskController::class, 'store']);
+    Route::post('/{id}/subtasks', [TaskController::class, 'storeSubtask']);
 });
