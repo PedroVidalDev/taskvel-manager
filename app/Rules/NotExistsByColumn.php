ae620416e487b2f4d05768b6eeb47d908216fccf
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-19 10:54:55 -0300

    feat: create rule for not exist

---
+++ app/Rules/NotExistsByColumn.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class NotExistsByColumn implements ValidationRule
+{
+
+    protected $name;
+    protected $model;
+    protected $column;
+
+    public function __construct($name, $model, $column)
+    {
+        $this->name = $name;
+        $this->model = $model;
+        $this->column = $column;
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if($this->model::where($this->column, $value)->exists()) {
+            $fail("The {$this->name} with column $this->column $value already exist.");
+        }
+    }
+}
