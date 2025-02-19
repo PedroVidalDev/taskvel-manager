56e9b5c14171566bb3d96bec4caa790625b7d340
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-19 10:57:47 -0300

    feat: create request and resource for user

---
+++ app/Http/Requests/User/LoginUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+use App\Rules\ExistsByColumn;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ["required", "string", "email", new ExistsByColumn("User", User::class, "email")],
+            'password' => ["required", "string", "min:8"],
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+use App\Rules\NotExistsByColumn;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ["required", "string", "min:3", "max:255"],
+            'email' => ["required", "string", "email", new NotExistsByColumn("User", User::class, "email")],
+            'password' => ["required", "string", "min:8"],
+        ];
+    }
+}
