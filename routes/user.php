19809899a79e80eb633781ed2456bf7e90c79225
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-19 11:20:31 -0300

    feat: create endpoint for get tasks

--- routes/user.php
+++ routes/user.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/auth')->group(function () {
+    Route::get('/users/{id}/tasks', [UserController::class, 'showTasks']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/register', [UserController::class, 'register']);
