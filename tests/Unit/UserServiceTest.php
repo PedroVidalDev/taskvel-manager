93b6f67a061d98f85699f9562cd35da41acdd355
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-26 17:46:58 -0300

    test: create mock for repository and for user informations

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -2,15 +2,32 @@
 
 namespace Tests\Unit;
 
+use App\Http\Repositories\UserRepository;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\TestCase;
 
 class UserServiceTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
+    use RefreshDatabase;
+
+    private UserService $service;
+    private $userRepositoryMock;
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->userRepositoryMock = \Mockery::mock(UserRepository::class);
+        $this->service = new UserService($this->userRepositoryMock);
+    }
+
+    private array $userMock = [
+        'id' => 1,
+        'name' => 'Pedro Vidal',
+        'email' => 'pedrovidal@gmail.com'
+    ];
+
+    public function test_example(): void {
+
     }
 }
