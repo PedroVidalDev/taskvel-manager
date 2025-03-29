0e28491aa3d368d7e28ffa318acccd549bc0aea3
PedroVidalDev <pedrohvidals@gmail.com>
2025-03-29 13:50:37 -0300

    feat: create project model

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\UserScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[ScopedBy([UserScope::class])]
+class Project extends Model
+{
+    protected $table = 'projects';
+
+    protected $fillable = ['name', 'user_id', 'created_at', 'updated_at'];
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class, 'project_id', 'id');
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
